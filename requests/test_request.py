44ec83aa8d127179ced19b15269e6f906e77d209
SebWae <s254120@dtu.dk>
2026-01-16 22:30:08 +0100

    script for requests exercises

---
+++ requests/test_request.py
@@ -0,0 +1,6 @@
+import requests
+
+pload = {'username':'Olivia','password':'123'}
+response = requests.post('https://httpbin.org/post', data = pload)
+print(response.status_code)
+print(response.content)
